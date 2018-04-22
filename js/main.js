bc8509ea66cbda2a73cacd9e04a9f7cbc75c6f73
simongt <simongt@users.noreply.github.com>
2018-04-22 16:00:18 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
-var cards = ['queen','queen','king','king'];
+var cards = [
+  {
+    rank: 'Queen',
+    suit: 'Hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    rank: 'Queen',
+    suit: 'Diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    rank: 'King',
+    suit: 'Hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    rank: 'King',
+    suit: 'Diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  },
+];
+
 var cardsInPlay = [];
 /*
-  Second, you'll create a function to group together the logic to check to see if the two cards that the user has flipped over match each other. You'll provide feedback to the user letting them know if the two cards match, or if they should try again.
+  Create a function to group together the logic to check to see if the two cards that the user has flipped over match each other. You'll provide feedback to the user letting them know if the two cards match, or if they should try again.
 */
 var checkForMatch = function() {
   if (cardsInPlay.length == 2) {
@@ -15,11 +37,12 @@ var checkForMatch = function() {
 };
 
 /*
-  First you'll create a function that will store all steps related to selecting, or flipping over, a card. When the user flips a card over, you'll want to add that card to the array of cards that are in play. If the user has flipped over two cards, you'll want to check for a match.
+  Create a function that will store all steps related to selecting, or flipping over, a card. When the user flips a card over, you'll want to add that card to the array of cards that are in play. If the user has flipped over two cards, you'll want to check for a match.
 */
 var flipCard = function(cardId) {
-  cardsInPlay.push(cards[cardId]);
-  console.log("User flipped " + cards[cardId] + ".");
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit + ".");
+  console.log("Card image: \"" + cards[cardId].cardImage + "\"");
   checkForMatch();
 };
 
