1f7c554c1e324ddf164830db563e0c6bd3ac292a
Sy <simon.tsegay@gmail.com>
2019-03-12 06:58:23 -0400

    animated simple fade-in for card entrance

--- js/main.js
+++ js/main.js
@@ -4,48 +4,49 @@ const cards = [
     rank: 'Queen',
     suit: 'Hearts',
     img_url: './images/queen-of-hearts.png',
-    isFlipped: false
   },
   {
     rank: 'Queen',
     suit: 'Diamonds',
     img_url: './images/queen-of-diamonds.png',
-    isFlipped: false
   },
   {
     rank: 'King',
     suit: 'Hearts',
     img_url: './images/king-of-hearts.png',
-    isFlipped: false
   },
   {
     rank: 'King',
     suit: 'Diamonds',
     img_url: './images/king-of-diamonds.png',
-    isFlipped: false
   },
 ];
 
 // card URLs are ./ and not ../ since path is relative to index.html
 const unFlippedCard = {
   img_url: './images/back.png',
-  isFlipped: false
 };
 
 let cardsInPlay = []; // game state
 
+let t; // timeout
+
 // Create a game board initially displaying the back of all cards (face down).
-(createBoard = () => {
+createBoard = () => {
   // Iterate through available set of cards, creating a DOM node for each one and assigning an image and ID to it.
-  const gameBoardNode = document.querySelectorAll('div.game-board')[0];
+  const gameBoardNode = document.querySelector('div.game-board');
+  shuffle(cards);
   cards.forEach((card, index) => {
-    const cardNode = document.createElement('img');
-    cardNode.setAttribute('src', unFlippedCard.img_url);
-    cardNode.setAttribute('data-id', index);
-    cardNode.addEventListener('click', () => flipCard(cardNode, card));
-    gameBoardNode.appendChild(cardNode);
+    t = setTimeout(() => {
+      const cardNode = document.createElement('img');
+      cardNode.setAttribute('src', unFlippedCard.img_url);
+      cardNode.setAttribute('data-id', index);
+      cardNode.classList.toggle('fade-in');
+      cardNode.addEventListener('click', () => flipCard(cardNode, card));
+      gameBoardNode.appendChild(cardNode);      
+    }, (index + 1) * 300);
   });
-})();
+}
 
 /*
   Selecting, or flipping over, a card... When the user flips a card over, you'll want to add that card to the array of cards that are in play. If the user has flipped over two cards, you'll want to check for a match.
@@ -63,10 +64,11 @@ flipCard = (cardNode, card) => {
   Check to see if the two cards that the user has flipped over match each other, and provide feedback to the user letting them know if the two cards match, or if they should try again.
 */
 checkForMatch = (cardNode, card) => {
-  pauseClicks();
+  pauseClicks(); // pause clicks once two cards are flipped over
   let resultMessage = '';
   if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
     resultMessage = 'You found cards with matching ranks!';
+    updateTicker(resultMessage);
   } else {
     const flippedCards = document.querySelectorAll('.card-flipped');
     flippedCards.forEach(flippedCard => {
@@ -74,14 +76,14 @@ checkForMatch = (cardNode, card) => {
       setTimeout(() => {
         flippedCard.classList.remove('card-flipped');
         flippedCard.setAttribute('src', unFlippedCard.img_url);
-        resumeClicks();
+        resumeClicks();  // resume once cards are flipped back
       }, 1000);
     });
   }
   resultMessage && setTimeout(() => {
     confirm(resultMessage + ' Play again?');
     resetBoard();
-    resumeClicks();
+    resumeClicks(); // resume clicks once game board is reset
   }, 250);
 };
 
@@ -99,16 +101,26 @@ resumeClicks = () => {
   });
 };
 
+updateTicker = (message) => {
+  const navbar = document.querySelector('p.game-result');
+  navbar.innerHTML = message;
+  const resetButton = document.createElement('button');
+  navbar.appendChild(resetButton);
+}
+
 /*
   Reset board: clear the cards in play and flip all cards back.
 */
 resetBoard = () => {
   cardsInPlay = [];
   cards.forEach((card, index) => {
-    // clear the game board
-    document.querySelectorAll('.game-board')[0].innerHTML = '';
-    shuffle(cards);
-    createBoard();
+    // clear the game board    
+    clearTimeout(t);
+    t = setTimeout(() => {
+      document.querySelector('.game-board').innerHTML = '';
+      shuffle(cards);
+      createBoard();
+    }, 500);
   });
 };
 
@@ -132,3 +144,5 @@ shuffle = (array) => {
   }
   return array;
 }
+
+createBoard();
