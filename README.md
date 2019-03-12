5e62316aabdf21f61008afecbee0a37d7a24ce50
Sy <simon.tsegay@gmail.com>
2019-03-12 02:02:04 -0400

    refactored codebase, improved logic + styling, added flexbox + es6 notation

--- README.md
+++ README.md
@@ -5,3 +5,18 @@ MemorEyes is a card game in which all of the cards are laid face down on a surfa
 ![MemorEyes](./images/memor-eyes.jpg)
 
 This simple game app was adapted from General Assembly's [Dash](https://dash.generalassemb.ly/) learning curriculum.
+
+## Features
+
+* Board resets per round with shuffled cards.
+* Result of each game is displayed within the prompt to play again.
+
+## Updates Needed
+
+* Detect when the same card is clicked twice, and tell user to pick again. ✓
+* Get the second card to show that it's flipped over. ✓
+* Disable card flipping after the game is over. ✓
+* Use modals instead of confirm alerts.
+* Add a reset button instead of a prompt.
+* Implement a way to keep track of and display the user's score.
+* Create breakpoints for widths > 640px, test for mobile responsiveness.
--- js/main.js
+++ js/main.js
@@ -1,127 +1,106 @@
-/*
-  Features added:
-    - Board resets with shuffled cards.
-    - Result of each game is displayed within the prompt to play again.
-  Updates needed:
-    - When the same card is clicked twice, tell user to pick again.
-    - Get the second card to show that it's flipped over.
-    - Add a reset button instead of a prompt.
-    - Disable card flipping after the game is over.
-    - Implement a way to keep track of and display the user's score.
-*/
-
-/*
-  Game demo consists of these 4 card objects
-*/
-var cards = [
+// Game demo consists of these 4 card objects
+const cards = [
   {
     rank: 'Queen',
     suit: 'Hearts',
-    cardImage: 'images/queen-of-hearts.png'
+    img_url: './images/queen-of-hearts.png',
+    isFlipped: false
   },
   {
     rank: 'Queen',
     suit: 'Diamonds',
-    cardImage: 'images/queen-of-diamonds.png'
+    img_url: './images/queen-of-diamonds.png',
+    isFlipped: false
   },
   {
     rank: 'King',
     suit: 'Hearts',
-    cardImage: 'images/king-of-hearts.png'
+    img_url: './images/king-of-hearts.png',
+    isFlipped: false
   },
   {
     rank: 'King',
     suit: 'Diamonds',
-    cardImage: 'images/king-of-diamonds.png'
+    img_url: './images/king-of-diamonds.png',
+    isFlipped: false
   },
 ];
 
-var cardsInPlay = [];
-
-/*
-  Check to see if the two cards that the user has flipped over match each other. You'll provide feedback to the user letting them know if the two cards match, or if they should try again.
-*/
-var checkForMatch = function() {
-  if (cardsInPlay.length == 2) {
-    var result;
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-      result = "You found a match!";
-    }
-    else {
-      result = "Sorry, try again.";
-    }
-    // Display result within prompt to play again
-    if(cardsInPlay.length == 2){
-      confirm(result + " Play again?");
-      // Update this so that if user clicks OK then it resets, otherwise stays on the page while no longer able to flip cards.
-      resetBoard();
-      // Better yet, just create a reset button!
-    }
-    // Display result in-page below cards
-    document.getElementById("gameResult").innerHTML = result;
-    // Display result to console
-    console.log(result);
-  }
+// card URLs are ./ and not ../ since path is relative to index.html
+const unFlippedCard = {
+  img_url: './images/back.png',
+  isFlipped: false
 };
+
+let cardsInPlay = []; // game state
+
+// Create a game board initially displaying the back of all cards (face down).
+(createBoard = () => {
+  // Iterate through available set of cards, creating a DOM node for each one and assigning an image and ID to it.
+  const gameBoardNode = document.querySelectorAll('div.game-board')[0];
+  cards.forEach((card, index) => {
+    const cardNode = document.createElement('img');
+    cardNode.setAttribute('src', unFlippedCard.img_url);
+    cardNode.setAttribute('data-id', index);
+    cardNode.addEventListener('click', () => flipCard(cardNode, card));
+    gameBoardNode.appendChild(cardNode);
+  });
+})();
 
 /*
   Selecting, or flipping over, a card... When the user flips a card over, you'll want to add that card to the array of cards that are in play. If the user has flipped over two cards, you'll want to check for a match.
 */
-var flipCard = function() {
-  var cardId = this.getAttribute('data-id');
-  cardsInPlay.push(cards[cardId].rank);
-  console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit + ".");
-  console.log("Card image: \'" + cards[cardId].cardImage + "\'");
-  this.setAttribute('src', cards[cardId].cardImage);
-  checkForMatch();
-  // Doesn't seem to work, trying to disable clicking on cards after game
-  // this.removeEventListener('click', flipCard);
+flipCard = (cardNode, card) => {
+  cardsInPlay.push(card);
+  cardNode.classList.add('card-flipped');
+  setTimeout(() => {
+    cardNode.setAttribute('src', card.img_url);
+    cardsInPlay.length === 2 && checkForMatch();
+  }, 250);
 };
 
 /*
-  Create a game board initially displaying the back of all cards (face down).
+  Check to see if the two cards that the user has flipped over match each other, and provide feedback to the user letting them know if the two cards match, or if they should try again.
 */
-var createBoard = function() {
-  /*
-    Iterate through available set of cards, creating a DOM node for each one and assigning an image and ID to it.
-  */
-  for (var i = 0; i < cards.length; i++) {
-    var cardElement = document.createElement('img');
-    cardElement.setAttribute('src', 'images/back.png');
-    cardElement.setAttribute('data-id', i);
-    cardElement.addEventListener('click', flipCard);
-    document.getElementById('game-board').appendChild(cardElement);
-    // Also works fine, alternatively...
-    // document.querySelector('#game-board').appendChild(cardElement);
+checkForMatch = () => {
+  let resultMessage;
+  if (cardsInPlay[0].suit === cardsInPlay[1].suit) {
+    resultMessage = "You found a match!";
+  } else {
+    resultMessage = "Sorry, try again.";
   }
+  setTimeout(() => {
+    confirm(resultMessage + "Play again?");
+    resetBoard();
+  }, 100);
 };
 
 /*
   Reset board: clear the cards in play and flip all cards back.
 */
-var resetBoard = function() {
+resetBoard = () => {
   cardsInPlay = [];
-  for (var i = 0; i < cards.length; i++) {
-    document.getElementById('game-board').innerHTML = '';
-    // shuffle cards and re-create the board
+  cards.forEach((card, index) => {
+    // clear the game board
+    document.querySelectorAll('.game-board')[0].innerHTML = '';
     shuffle(cards);
     createBoard();
-  }
+  });
 };
 
-/*
-  Array shuffler, credit: Fisher-Yates (Knuth) Shuffle
-*/
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
+/**
+ * Array shuffler, credit: Fisher-Yates (Knuth) Shuffle
+ * https://stackoverflow.com/a/2450976/1293256
+ * @param  {Array} array The array to shuffle
+ * @return {String}      The first item in the shuffled array
+ */
+shuffle = (array) => {
+  let currentIndex = array.length, temporaryValue, randomIndex;
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-
     // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
@@ -129,5 +108,3 @@ function shuffle(array) {
   }
   return array;
 }
-
-createBoard();
